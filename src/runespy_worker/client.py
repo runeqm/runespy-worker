005e8d26f7a5f1482cff36a4f00bc97e87f7ea44
Mario Lucassen <mario.lucassen@hotmail.com>
2026-04-11 16:26:11 +0200

    Reduce worker batch flush latency

--- src/runespy_worker/client.py
+++ src/runespy_worker/client.py
@@ -63,7 +63,7 @@ from runespy_worker.protocol import build_message
 
 logger = logging.getLogger("runespy_worker")
 
-BATCH_FLUSH_INTERVAL = 5.0  # seconds between batch sends
+BATCH_FLUSH_INTERVAL = 2.0  # seconds between batch sends
 BATCH_MAX_SIZE = 100        # send early if this many results are queued
 
 
